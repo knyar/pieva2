3a8b524c288ff5cabc0e1bc587ef1767040af075
Anton Tolchanov <me@knyar.net>
2015-08-23 13:02:26 +0100

    Stricter types in pieva bibliopixel driver

--- pbdriver.pyx
+++ pbdriver.pyx
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import cython
 from pieva import *
 from screen import Screen
 import numpy as np
@@ -6,48 +7,78 @@ cimport numpy as np
 
 from bibliopixel.drivers.driver_base import DriverBase
 
-class DriverPieva(DriverBase):
+cdef class DriverPievaStruct:
+    cdef np.int_t height
+    cdef np.int_t width
+    cdef np.int_t pixels
 
-    def __init__(self, int num=0, int width=0, int height=0):
+class DriverPieva(DriverBase, DriverPievaStruct):
+
+    def __init__(self, np.int_t num=0, np.int_t width=0, np.int_t height=0):
         super(DriverPieva, self).__init__(num, width, height)
         self.screen = Screen(sections)
         self.screen.dimm(0)
         self.pixels = width * height
         self.width = width
         self.height = height
-        self.bitmap = np.zeros([140, 140])
 
+    @cython.boundscheck(False)
     def update(self, data):
         if len(data) != self.pixels * 3:
             print "Data length mismatch"
             return
 
-        cdef int i, r, g, b, rgb
+        cdef np.ndarray[np.int_t, ndim=2] bitmap = np.zeros([140, 140], dtype='int')
+        cdef np.int_t i, r, g, b, rgb
         for i in range(self.pixels):
             r = data[i * 3 + 0]
             g = data[i * 3 + 1]
             b = data[i * 3 + 2]
             rgb = (r << 16) + (g << 8) + b
-            self.draw(self.bitmap, i, rgb)
-        self.screen.send(self.bitmap)
-
-    def draw(self, bitmap, int i, int rgb):
-        bitmap[i % self.width][i / self.height] = rgb
+            bitmap[i % self.width][i / self.height] = rgb
+        self.screen.send(bitmap)
 
 class DriverPievaX4(DriverPieva):
-    def draw(self, bitmap, int i, int rgb):
-        cdef int iwidth = i % self.width
-        cdef int iheight = i / self.width
-        bitmap[iwidth][iheight] = rgb
-        bitmap[iheight][139 - iwidth] = rgb
-        bitmap[139 - iheight][iwidth] = rgb
-        bitmap[139 - iwidth][139 - iheight] = rgb
+    @cython.boundscheck(False)
+    def update(self, data):
+        if len(data) != self.pixels * 3:
+            print "Data length mismatch"
+            return
+
+        cdef np.ndarray[np.int_t, ndim=2] bitmap = np.zeros([140, 140], dtype='int')
+        cdef np.int_t i, r, g, b, rgb, iwidth, iheight
+        for i in range(self.pixels):
+            r = data[i * 3 + 0]
+            g = data[i * 3 + 1]
+            b = data[i * 3 + 2]
+            rgb = (r << 16) + (g << 8) + b
+            iwidth = i % self.width
+            iheight = i / self.width
+            bitmap[iwidth, iheight] = rgb
+            bitmap[iheight, 139 - iwidth] = rgb
+            bitmap[139 - iheight, iwidth] = rgb
+            bitmap[139 - iwidth, 139 - iheight] = rgb
+
+        self.screen.send(bitmap)
 
 class DriverPievaX4Rev(DriverPieva):
-    def draw(self, bitmap, int i, int rgb):
-        cdef int iwidth = i % self.width
-        cdef int iheight = i / self.height
-        bitmap[70 - iwidth][70 - iheight] = rgb
-        bitmap[70 - iheight][70 + iwidth] = rgb
-        bitmap[70 + iheight][70 - iwidth] = rgb
-        bitmap[70 + iwidth][70 + iheight] = rgb
+    @cython.boundscheck(False)
+    def update(self, data):
+        if len(data) != self.pixels * 3:
+            print "Data length mismatch"
+            return
+
+        cdef np.ndarray[np.int_t, ndim=2] bitmap = np.zeros([140, 140], dtype='int')
+        cdef np.int_t i, r, g, b, rgb, iwidth, iheight
+        for i in range(self.pixels):
+            r = data[i * 3 + 0]
+            g = data[i * 3 + 1]
+            b = data[i * 3 + 2]
+            rgb = (r << 16) + (g << 8) + b
+            iwidth = i % self.width
+            iheight = i / self.height
+            bitmap[70 - iwidth, 70 - iheight] = rgb
+            bitmap[70 - iheight, 70 + iwidth] = rgb
+            bitmap[70 + iheight, 70 - iwidth] = rgb
+            bitmap[70 + iwidth, 70 + iheight] = rgb
+        self.screen.send(bitmap)
